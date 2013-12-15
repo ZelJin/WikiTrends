16622fd5abee5f89a5f67b0caa43d5a0754306a9
Dmitry Zeldin <zeldind@gmail.com>
2013-12-15 17:07:36 +0400

    UI improvements

--- WikiTrends.rb
+++ WikiTrends.rb
@@ -5,21 +5,65 @@ require 'sinatra/content_for'
 require_relative 'api.rb'
 
 get '/' do
-  points = [
-      {key: "2013-01-01", value: 500},
-      {key: "2013-01-02", value: 430},
-      {key: "2013-01-03", value: 570},
-      {key: "2013-01-04", value: 1020},
-      {key: "2013-01-05", value: 300},
-      {key: "2013-01-06", value: 870},
-      {key: "2013-01-07", value: 600},
-      {key: "2013-01-08", value: 730},
-      {key: "2013-01-09", value: 510},
-      {key: "2013-01-10", value: 940},
-      {key: "2013-01-11", value: 800},
-      {key: "2013-01-12", value: 700},
-      {key: "2013-01-13", value: 215}]
-  haml :index, locals: {points: points}
+  client = MongoClient.new('localhost', 27017)
+  db = client["wikitrends"]
+  db.authenticate("wikitrends", "wiki_admin_passwd")
+  @trends = db["trends"].find.sort({diff: -1}).limit(5).to_a
+  @views = {}
+  @news = {}
+  @trends.each do |trend|
+    @views[trend["name"]] = db["views"].find({name: trend["name"]})
+    @news[trend["name"]] = request_news(trend["name"])
+  end
+  haml :index
+end
+
+get '/daily' do
+  client = MongoClient.new('localhost', 27017)
+  db = client["wikitrends"]
+  db.authenticate("wikitrends", "wiki_admin_passwd")
+  @trends = db["trends"].find.sort({diff: -1}).limit(5).to_a
+  puts "Trends"
+  @views = {}
+  @news = {}
+  @trends.each do |trend|
+    puts "I've been there"
+    @views[trend["name"]] = db["views"].find({name: trend["name"]})
+    @news[trend["name"]] = request_news(trend["name"])
+  end
+  haml :index
+end
+
+get '/weekly' do
+  client = MongoClient.new('localhost', 27017)
+  db = client["wikitrends"]
+  db.authenticate("wikitrends", "wiki_admin_passwd")
+  @trends = db["trends"].find.sort({diff: -1}).limit(5).to_a
+  puts "Trends"
+  @views = {}
+  @news = {}
+  @trends.each do |trend|
+    puts "I've been there"
+    @views[trend["name"]] = db["views"].find({name: trend["name"]})
+    @news[trend["name"]] = request_news(trend["name"])
+  end
+  haml :index
+end
+
+get '/monthly' do
+  client = MongoClient.new('localhost', 27017)
+  db = client["wikitrends"]
+  db.authenticate("wikitrends", "wiki_admin_passwd")
+  @trends = db["trends"].find.sort({diff: -1}).limit(5).to_a
+  puts "Trends"
+  @views = {}
+  @news = {}
+  @trends.each do |trend|
+    puts "I've been there"
+    @views[trend["name"]] = db["views"].find({name: trend["name"]})
+    @news[trend["name"]] = request_news(trend["name"])
+  end
+  haml :index
 end
 
 get '/api' do
@@ -30,20 +74,12 @@ post '/api' do
   haml :api_result, locals: {data: request_views(params[:post][:name])}
 end
 
-get '/rss' do
-  haml :rss
-end
-
-post '/rss' do
-  haml :rss_result, locals: {feed: get_rss_feed(params[:post][:name])}
-end
-
 get '/news' do
   haml :news
 end
 
 post '/news' do
-  haml :news_result, locals: {feed: get_news(params[:post][:name])}
+  haml :news_result, locals: {feed: request_news(params[:post][:name])}
 end
 
 get '/parse' do
@@ -54,13 +90,11 @@ get '/views' do
   client = MongoClient.new('localhost', 27017)
   db = client["wikitrends"]
   db.authenticate("wikitrends", "wiki_admin_passwd")
-  collection = db["articles"]
-  articles = collection.find
-  collection = db["views"]
+  articles = db["articles"].find
   i = 0
   count = articles.count
   articles.each do |article|
-    parse_article_views(article["name"], collection)
+    parse_views(article["name"], db["views"])
     puts "#{i += 1} out of #{count}"
   end
 end
--- api.rb
+++ api.rb
@@ -4,7 +4,7 @@ require 'mongo'
 require 'rss'
 include Mongo
 
-#API helpers
+#helpers
 helpers do
   def request_views(name)
     response = RestClient.get "http://stats.grok.se/json/en/latest90/#{URI.escape(name.tr(" ", "_"))}"
@@ -20,37 +20,27 @@ helpers do
     client = MongoClient.new('localhost', 27017)
     db = client["wikitrends"]
     db.authenticate("wikitrends", "wiki_admin_passwd")
-    collection = db["articles"]
     page.css('.wikitable a').each do |element|
       if element[:title] != nil
-        collection.update({name: element[:title]}, {name: element[:title], parse_date: Time.now.utc}, {upsert: true})
+        db["articles"].update({name: element[:title]}, {name: element[:title], parse_date: Time.now.utc}, {upsert: true})
       end
     end
   end
 
-  def parse_article_views(name, collection)
+  def parse_views(name, collection)
     daily_views = request_views(name)
     daily_views.each do |key, value|
-      collection.insert({name: name, date: key},{name: name, date: key, views: value}, {upsert: true})
+      collection.insert({name: name, date: key}, {name: name, date: key, views: value}, {upsert: true})
     end
   end
 
-  def get_rss_feed(request)
-    response = RestClient.get("http://news.google.com/news?q=#{URI.escape(request)}&output=rss")
-    if response.code == 200
-      RSS::Parser.parse(response)
-    else
-      response.code
-    end
-  end
-
-  def get_news(request)
+  def request_news(request)
     response = RestClient.get("https://ajax.googleapis.com/ajax/services/search/news?v=1.0&q=#{URI.escape(request)}")
     if response.code == 200
-      puts response
-      JSON.parse(response)["responseData"]["results"]
+      JSON.parse(response)["responseData"]["results"][0..2]
     else
       response.code
     end
   end
+
 end
