0c764f6c52f9d2696da23368448b5c3a92ddec51
Evil-Crab <evil.crab@gmail.com>
2013-12-16 13:39:47 +0400

    Map-Reduce

---
+++ map_reduce/days.js
@@ -0,0 +1,114 @@
+var mapWeekAvgFunction = function() {
+    var parseDate = function(date_string){
+        var date_parts = date_string.split("-");
+        return new Date(date_parts[0], (date_parts[1]-1), date_parts[2])
+    }
+
+    Date.prototype.getWeek = function(){
+        var first_jan = new Date(this.getFullYear(), 0, 1);
+        return Math.ceil((((this-first_jan)/86400000)+first_jan.getDay()+1)/7)
+    }
+    var date = parseDate(this.date);
+
+    var key = {
+        name: this.name,
+        week: date.getWeek()
+    };
+
+    var value ={
+        views: this.views,
+        count: 1,
+        days: [{
+            date: this.date,
+            views: this.views
+        }]};
+    emit(key, value);
+};
+
+var reduceWeekAvgFunction = function(key, values) {
+    reducedVal = {
+        views: 0,
+        count: 0,
+        days: []};
+
+    for (var idx = 0; idx < values.length; idx++) {
+        reducedVal.views += values[idx].views;
+        reducedVal.count += values[idx].count;
+        for(var idx2 = 0; idx2 < values[idx].days.length; idx2++){
+            reducedVal.days.push(values[idx].days[idx2]);
+        }
+    }
+
+    return reducedVal;
+};
+
+var finalizeWeekAvgFunction = function (key, reducedVal) {
+
+    reducedVal.avg = reducedVal.views/reducedVal.count;
+
+    return reducedVal;
+
+};
+
+db.views.mapReduce(
+    mapWeekAvgFunction,
+    reduceWeekAvgFunction,
+    {
+        out: { replace: "tmp_weekAvg" },
+        finalize: finalizeWeekAvgFunction
+    }
+)
+
+var mapDaysDiffFunction = function() {
+
+    for(var idx = 0; idx < this.value.days.length; idx++){
+        var key = {
+            name: this._id.name,
+            date: this.value.days[idx].date
+        };
+
+        var value ={
+            type: 'daily',
+            diff: this.value.days[idx].views - this.value.avg
+        }
+        emit(key, value);
+    }
+};
+
+var reduceDaysDiffFunction = function(key, values) {
+    reducedVal = {
+        type: '',
+        diff: 0
+    };
+
+    for (var idx = 0; idx < values.length; idx++) {
+        reducedVal.type = values[idx].type;
+        reducedVal.diff = values[idx].diff;
+    }
+
+    return reducedVal;
+};
+
+db.tmp_weekAvg.mapReduce(
+    mapDaysDiffFunction,
+    reduceDaysDiffFunction,
+    {
+        out: { replace: "tmp_daysDiff" }
+    }
+)
+
+var cursor = db.tmp_daysDiff.find();
+var record;
+while(cursor.hasNext()) {
+    record = cursor.next();
+    data = {
+        name: record._id.name,
+        type: record.value.type,
+        diff: record.value.diff,
+        valid_date: record._id.date
+    }
+    db.trends.insert(data);
+}
+
+db.tmp_weekAvg.drop();
+db.tmp_daysDiff.drop();
---
+++ map_reduce/weeks.js
@@ -0,0 +1,173 @@
+var mapWeeksFunction = function() {
+    var parseDate = function(date_string){
+        var date_parts = date_string.split("-");
+        return new Date(date_parts[0], (date_parts[1]-1), date_parts[2])
+    }
+
+    var getMonday = function (date) {
+
+        var now = date? new Date(date) : new Date();
+        now.setHours(0,0,0,0);
+
+        var monday = new Date(now);
+        monday.setDate(monday.getDate() - monday.getDay() + 1);
+
+        return monday;
+    }
+
+    var printDate = function(date) {
+        year = date.getFullYear().toString();
+        month = (date.getMonth() + 1);
+        month = month  >= 10 ? month.toString() : '0' + month.toString();
+        day = (date.getDate());
+        day = day  >= 10 ? day.toString() : '0' + day.toString();
+        return year + '-' +
+               month + '-' +
+               day;
+    }
+
+    var date = parseDate(this.date);
+    var week = getMonday(date);
+    var key = {
+        name: this.name,
+        week: printDate(week)
+    };
+
+    var value ={
+        views: this.views
+    }
+    emit(key, value);
+};
+
+var reduceWeeksFunction = function(key, values) {
+    reducedVal = {
+        views: 0};
+
+    for (var idx = 0; idx < values.length; idx++) {
+        reducedVal.views += values[idx].views;
+    }
+
+    return reducedVal;
+};
+
+db.views.mapReduce(
+    mapWeeksFunction,
+    reduceWeeksFunction,
+    {
+        out: { replace: "tmp_weeks" }
+    }
+)
+
+var mapMonthAvgFunction = function() {
+    var parseDate = function(date_string){
+        var date_parts = date_string.split("-");
+        return new Date(date_parts[0], (date_parts[1]-1), date_parts[2])
+    }
+
+    var date = parseDate(this._id.week);
+
+    var key = {
+        name: this._id.name,
+        month: date.getMonth()
+    };
+
+    var value ={
+        views: this.value.views,
+        count: 1,
+        weeks: [{
+            week: this._id.week,
+            views: this.value.views
+        }]};
+
+    emit(key, value);
+};
+
+var reduceMonthAvgFunction = function(key, values) {
+    reducedVal = {
+        views: 0,
+        count: 0,
+        weeks: []};
+
+    for (var idx = 0; idx < values.length; idx++) {
+        reducedVal.views += values[idx].views;
+        reducedVal.count += values[idx].count;
+        for(var idx2 = 0; idx2 < values[idx].weeks.length; idx2++){
+            reducedVal.weeks.push(values[idx].weeks[idx2]);
+        }
+    }
+
+    return reducedVal;
+};
+
+var finalizeMonthAvgFunction = function (key, reducedVal) {
+
+    reducedVal.avg = reducedVal.views/reducedVal.count;
+
+    return reducedVal;
+
+};
+
+db.tmp_weeks.mapReduce(
+    mapMonthAvgFunction,
+    reduceMonthAvgFunction,
+    {
+        out: { replace: "tmp_monthAvg" },
+        finalize: finalizeMonthAvgFunction
+    }
+)
+
+var mapWeeksDiffFunction = function() {
+
+    for(var idx = 0; idx < this.value.weeks.length; idx++){
+        var key = {
+            name: this._id.name,
+            date: this.value.weeks[idx].week
+        };
+
+        var value ={
+            type: 'weekly',
+            diff: this.value.weeks[idx].views - this.value.avg
+        }
+        emit(key, value);
+    }
+};
+
+var reduceWeeksDiffFunction = function(key, values) {
+    reducedVal = {
+        type: '',
+        diff: 0
+    };
+
+    for (var idx = 0; idx < values.length; idx++) {
+        reducedVal.type = values[idx].type;
+        reducedVal.diff = values[idx].diff;
+    }
+
+    return reducedVal;
+};
+
+
+db.tmp_monthAvg.mapReduce(
+    mapWeeksDiffFunction,
+    reduceWeeksDiffFunction,
+    {
+        out: { replace: "tmp_weeksDiff" }
+    }
+)
+
+var cursor = db.tmp_weeksDiff.find();
+var record;
+while(cursor.hasNext()) {
+    record = cursor.next();
+    data = {
+        name: record._id.name,
+        type: record.value.type,
+        diff: record.value.diff,
+        valid_date: record._id.date
+    }
+    db.trends.insert(data);
+}
+
+db.tmp_weeks.drop();
+db.tmp_monthAvg.drop();
+db.tmp_weeksDiff.drop();
